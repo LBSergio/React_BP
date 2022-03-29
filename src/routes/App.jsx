ab1e62b7b3c0949ea388297c0b0d4954f1b2aa58
LBSergio <1999lopezbautistasergio@gmail.com>
2022-03-29 13:23:35 +0200

    First steps on how to navigate between pages

---
+++ src/routes/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';´
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Layout } from '@containers/layout';
+import { Login } from '@containers/login';
+import { RecoveryPassword } from '@containers/recoveryPassword';
+import '@styles/global.css'
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Layout>
+                    <Route exact path='/' component={ Home }/>
+                    <Route exact path='/login' component={ Login }/>
+                    <Route exact path='/recovery-password' component={ RecoveryPassword }/>
+                    <Route component={ NotFound }/>
+                </Layout>
+            </Switch>            
+        </BrowserRouter>
+    );
+}
+
+export { App };
